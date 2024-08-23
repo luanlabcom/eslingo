ae39d017163c8c76ce9e576e3d5b0224bdca7dde
Luan Tran <luanlabcom@gmail.com>
2024-08-23 13:33:22 +0700

    Intro custom component: remove mb-16, add border-b-2

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,7 +3,7 @@ import Controls from "./Controls";
 
 export default function Intro(props) {
     return (
-        <div className="mb-16 font-bold">
+        <div className="border-b-2 font-bold">
             <Text content={props.content} />
             <img className="my-4" src={ props.content.img } alt="" />            
             <Controls content={props.content} />
