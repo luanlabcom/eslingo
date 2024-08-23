67445c7ecfda686e23322dffa60afec2f3ed1aed
Luan Tran <luanlabcom@gmail.com>
2024-08-23 10:20:01 +0700

    Create custom component Controls

---
+++ src/components/Controls.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+
+export default function Controls(props) {
+    const translationRef = React.useRef(null)
+
+    function displayTranslation() {
+        translationRef.current.classList.toggle("hidden")
+    }
+    return (
+        <>
+            <div className="flex justify-between text-xs my-4">                    
+                <button 
+                    className="underline" 
+                    onClick={displayTranslation}
+                >Show translation</button>
+
+                <button>Replay</button>
+            </div>
+
+            <div className="hidden" ref={translationRef}>                    
+                <p className="text-blue-700">{ props.content.text.translationText }</p>
+            </div>
+        </>
+    )
+}
