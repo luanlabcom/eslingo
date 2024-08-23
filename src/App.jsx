b5fe0955a8b7ea4b4077e8f4b4903b15c670580e
Luan Tran <luanlabcom@gmail.com>
2024-08-23 13:32:07 +0700

    Create button Continue

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import Answer from "./components/Answer"
 export default function App() {
 
     return(
-        <div className="flex flex-col max-w-md mx-auto min-h-screen p-4">
-            <Intro content={data[0]} />
-            <Narrative content={data[1]} />
-            <Question content={data[2]} />
-            <Answer content={data[3]} />
-            <Dialogue content={data[4]} />
+        <div className="flex flex-col gap-4 max-w-md mx-auto min-h-screen p-4">
+                <Intro content={data[0]} />
+                <Narrative content={data[1]} />
+                <Question content={data[2]} />
+                <Answer content={data[3]} />
+                <Dialogue content={data[4]} />
+            <div className="flex justify-center mt-4">
+                <button className="rounded-full bg-neutral-950 px-12 py-2 font-semibold text-neutral-50">Continue</button>
+            </div>
         </div>
     )
 }
