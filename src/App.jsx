7b9478aa9d1050eace0315ec6d2b532429b97126
Luan Tran <luanlabcom@gmail.com>
2024-08-23 13:10:20 +0700

    Import Question component to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Dialogue from "./components/Dialogue"
 import Intro from "./components/Intro"
 import data from "../data"
 import Narrative from "./components/Narrative"
+import Question from "./components/Question"
 
 export default function App() {
 
@@ -9,6 +10,7 @@ export default function App() {
         <div className="max-w-md mx-auto min-h-screen p-4">
             <Intro content={data[0]} />
             <Narrative content={data[1]} />
+            <Question content={data[2]} />
             <Dialogue content={data[4]} />
         </div>
     )
