8660f5e43abbe95eb235803c1a28d394ebfa0158
Luan Tran <luanlabcom@gmail.com>
2024-08-23 12:54:09 +0700

    Revert "Import Narrative to App"
    This reverts commit 2166a6e0f2562e3ce5acf88ab35440ff2f916afd.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import Dialogue from "./components/Dialogue"
 import Intro from "./components/Intro"
 import data from "../data"
-import Narrative from "./components/Narrative"
 
 export default function App() {
 
     return(
         <div className="max-w-md mx-auto min-h-screen p-4">
             <Intro content={data[0]} />
-            <Narrative content={data[1]} />
             <Dialogue content={data[4]} />
         </div>
     )
