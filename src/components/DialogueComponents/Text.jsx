111603c65f0a38fccdeedc188a7c42bdda503523
Luan Tran <luanlabcom@gmail.com>
2024-08-23 12:24:57 +0700

    Text component: add flex-wrap, justify-center on each sentence; remove mt-4 on each word

--- src/components/DialogueComponents/Text.jsx
+++ src/components/DialogueComponents/Text.jsx
@@ -4,14 +4,14 @@ export default function Text(props) {
 
     const textElements = ipaTextArr.map((ipaText, index) => {
         return (
-            <div key={crypto.randomUUID()} className="flex flex-col mt-4 text-center">
+            <div key={crypto.randomUUID()} className="flex flex-col text-center">
                 <span className="text-xs text-red-600">{ipaText}</span>
                 <span className="text-2xl text-neutral-800">{targetLangTextArr[index]}</span>
             </div>
         )
     })    
     return(
-        <div className="flex gap-3">
+        <div className="flex gap-3 flex-wrap justify-center">
             { textElements }
         </div>
     )
