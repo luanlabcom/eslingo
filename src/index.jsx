5b768edb9ee2be180fedee5d9240de74654265e6
Luan Tran <luanlabcom@gmail.com>
2024-08-23 10:27:17 +0700

    Add App component to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
-ReactDOM.createRoot(document.getElementById("root")).render(<h1>Hello</h1>)
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+    <React.StrictMode>
+       <App />
+    </React.StrictMode>
+)
