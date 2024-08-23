68ce8a7e9b595e8f90d13647839223963084eee4
Luan Tran <luanlabcom@gmail.com>
2024-08-23 12:09:26 +0700

    Add Intro to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Dialogue from "./components/Dialogue"
+import Intro from "./components/Intro"
 import data from "../data"
 
 export default function App() {
-    const content = data
+
     return(
-        <div className="bg-neutral-400 max-w-md mx-auto min-h-screen p-4">
-            <Dialogue content={content} />
+        <div className="max-w-md mx-auto min-h-screen p-4">
+            <Intro content={data[0]} />
+            <Dialogue content={data[4]} />
         </div>
     )
 }
