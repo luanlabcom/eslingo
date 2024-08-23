2a12ba61c31bfba1228b9ae38645f57269a1bec8
Luan Tran <luanlabcom@gmail.com>
2024-08-23 14:08:46 +0700

    Modify style of cover image of Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,7 +5,7 @@ export default function Intro(props) {
     return (
         <div className="border-b-2 font-bold">
             <Text content={props.content} />
-            <img className="my-4" src={ props.content.img } alt="" />            
+            <img className="my-4 w-72 mx-auto" src={ props.content.img } alt="" />
             <Controls content={props.content} />
         </div>
     )
