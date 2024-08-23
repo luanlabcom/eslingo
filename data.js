7efe0792462ff16022569bea4ac782486585c079
Luan Tran <luanlabcom@gmail.com>
2024-08-23 12:03:27 +0700

    Add new data to data.js

--- data.js
+++ data.js
@@ -1,17 +1,35 @@
-export default {
-  id: 0,
-  type: "dialogue",
-  speakerInfo: {
-    avatar: "/public/liming.jpeg",
-    ipaName: "Tiánzhōng",
-    targetLangName: "田中",
+export default [
+  {
+    id: 0,
+    type: "intro",
+    speakerInfo: null,
+    timeline: [0, 1.5],
+    text: {
+      ipaText: "Dì|èr|kè|: |tīng |hé |huídá",
+      targetLangText: "第|二|课|: |听|和|回答",
+      translationText: "Lesson 2: Listen and Answer",
+    },
+    img: "/public/liming.jpeg",
+    colors: ["bg-neutral-50", "text-neutral-800"],
   },
-  timeline: [0, 3.459],
-  text: {
-    ipaText: "Nǐmen |hǎo|!",
-    targetLangText: "你们|好|!",
-    translationText: "Tanaka: Hello guys!",
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  {
+    id: 4,
+    type: "dialogue",
+    speakerInfo: {
+      avatar: "/public/liming.jpeg",
+      ipaName: "Tiánzhōng",
+      targetLangName: "田中",
+    },
+    timeline: [0, 3.459],
+    text: {
+      ipaText: "Nǐmen |hǎo|!",
+      targetLangText: "你们|好|!",
+      translationText: "Tanaka: Hello guys!",
+    },
+    img: "",
+    colors: ["bg-neutral-400", "text-neutral-800"],
   },
-  img: "",
-  colors: ["bg-neutral-400", "text-neutral-800"],
-};
+];
