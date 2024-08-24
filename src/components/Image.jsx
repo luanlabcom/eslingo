d42828006d4a77348aad8b49e6602c554ebce2ad
Luan Tran <luanlabcom@gmail.com>
2024-08-24 13:54:58 +0700

    Image custom component: Remove console.log

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,4 @@
 export default function Image(props) {
-    console.log("../assets/images/" + props.src)
     
     return (
         <img className="my-4 w-72 mx-auto" src={ "/assets/images/" + props.src } alt={ props.alt } />
