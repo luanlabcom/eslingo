ae457a4dcb8df1cc0ff7cef87b88b202f6b9bab4
Luan Tran <luanlabcom@gmail.com>
2024-08-24 13:41:21 +0700

    Change audio src path to ./public/audio

--- src/components/AuidoPlayer.jsx
+++ src/components/AuidoPlayer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const AudioPlayer = React.forwardRef((props, ref) => {
     return (        
         <audio ref={ref} onTimeUpdate={props.handlePausedTime}>
-            <source src="/chapter-2-qa.mp3" type="audio/mpeg" />
+            <source src="/assets/audio/chapter-2-qa.mp3" type="audio/mpeg" />
         </audio>       
     )
 })
