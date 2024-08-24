7a1f7e28e1f531c315b7231487c9f72088528ad6
Luan Tran <luanlabcom@gmail.com>
2024-08-24 13:42:18 +0700

    Change image src path to ./public/assets/images

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,7 @@
 export default function Image(props) {
+    console.log("../assets/images/" + props.src)
+    
     return (
-        <img className="my-4 w-72 mx-auto" src={ props.src } alt={ props.alt } />
+        <img className="my-4 w-72 mx-auto" src={ "/assets/images/" + props.src } alt={ props.alt } />
     )
 }
