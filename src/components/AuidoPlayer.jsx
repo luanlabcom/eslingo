1a344d708003c5385fc9a7fd944edb860c2e11a6
Luan Tran <luanlabcom@gmail.com>
2024-08-24 07:47:24 +0700

    AudioPlayer: change src type of mp3 to audio/mpeg

--- src/components/AuidoPlayer.jsx
+++ src/components/AuidoPlayer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const AudioPlayer = React.forwardRef((props, ref) => {
     return (        
         <audio ref={ref} onTimeUpdate={props.handlePausedTime}>
-            <source src="/chapter-2-qa.mp3" type="audio/mp3" />
+            <source src="/chapter-2-qa.mp3" type="audio/mpeg" />
         </audio>       
     )
 })
