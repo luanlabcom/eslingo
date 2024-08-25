2e8a11f50597a9f017d8c1d0b07386ebd8bc3cb4
Luan Tran <luanlabcom@gmail.com>
2024-08-25 12:27:52 +0700

    Update chapter 2 qa data

--- src/data/data-chapter-2-qa.js
+++ src/data/data-chapter-2-qa.js
@@ -3,10 +3,10 @@ export default [
     id: 0,
     type: "intro",
     speakerInfo: null,
-    timeline: [0, 3.459],
+    timeline: [0, 2.091],
     text: {
-      ipaText: "Dì|èr|kè| : |tīng |hé |huídá",
-      targetLangText: "第|二|课。|: |听|和|回答",
+      ipaText: "Dì|èr|kè| : |tīng |hé |huídá.",
+      targetLangText: "第|二|课|: |听|和|回答。",
       translationText: "Lesson 2: Listen and Answer",
     },
     img: {
@@ -19,11 +19,11 @@ export default [
     id: 1,
     type: "narrative",
     speakerInfo: null,
-    timeline: [3.459, 6.154],
+    timeline: [2.091, 4.798],
     text: {
-      ipaText: "“Dīng dōng”| — |Ménlíng|xiǎngle|.",
-      targetLangText: "“叮咚”| — |门铃|响了|。",
-      translationText: "“Ding dong” — The doorbell rang.",
+      ipaText: "“Dīng dōng”|... |ménlíng|xiǎngle|.",
+      targetLangText: "“叮咚”|。。。|门铃|响了|。",
+      translationText: "“Ding dong”... the doorbell rang.",
     },
     img: {
       src: "doorbell.jpeg",
@@ -35,7 +35,7 @@ export default [
     id: 2,
     type: "question",
     speakerInfo: null,
-    timeline: [6.154, 8.204],
+    timeline: [4.798, 6.397],
     text: {
       ipaText: "Ménlíng|xiǎngle|ma|?",
       targetLangText: "门铃|响了|吗|？",
@@ -51,7 +51,7 @@ export default [
     id: 3,
     type: "answer",
     speakerInfo: null,
-    timeline: [8.204, 10.874],
+    timeline: [6.397, 7.994],
     text: {
       ipaText: "Duì|,|ménlíng|xiǎngle|.",
       targetLangText: "对|，|门铃|响了|。",
@@ -67,10 +67,10 @@ export default [
     id: 4,
     type: "question",
     speakerInfo: null,
-    timeline: [10.874, 14.350999999999999],
+    timeline: [8.409, 11.529],
     text: {
       ipaText: "Ménlíng|xiǎngle|háishì|diànhuà|xiǎngle|?",
-      targetLangText: "门铃|响了|还是|电话|响了|？",
+      targetLangText: "门铃|响了，|还是，|电话|响了|？",
       translationText: "Did the doorbell ring or did the telephone ring?",
     },
     img: {
@@ -83,7 +83,7 @@ export default [
     id: 5,
     type: "answer",
     speakerInfo: null,
-    timeline: [14.350999999999999, 16.21],
+    timeline: [11.529, 12.939],
     text: {
       ipaText: "Ménlíng|xiǎngle|.",
       targetLangText: "门铃|响了|。",
@@ -99,7 +99,7 @@ export default [
     id: 6,
     type: "question",
     speakerInfo: null,
-    timeline: [16.21, 18.238],
+    timeline: [12.939, 14.317],
     text: {
       ipaText: "Shénme|xiǎngle|?",
       targetLangText: "什么|响了|？",
@@ -115,7 +115,7 @@ export default [
     id: 7,
     type: "answer",
     speakerInfo: null,
-    timeline: [18.238, 20.249],
+    timeline: [14.317, 15.711],
     text: {
       ipaText: "Ménlíng|xiǎngle|.",
       targetLangText: "门铃|响了|。",
@@ -131,7 +131,7 @@ export default [
     id: 8,
     type: "question",
     speakerInfo: null,
-    timeline: [20.249, 22.299],
+    timeline: [15.711, 17.197],
     text: {
       ipaText: "Ménlíng|huàile|ma|?",
       targetLangText: "门铃|坏了|吗|？",
@@ -147,7 +147,7 @@ export default [
     id: 9,
     type: "answer",
     speakerInfo: null,
-    timeline: [22.299, 27.043],
+    timeline: [17.197, 20.59],
     text: {
       ipaText: "Bù|,|ménlíng|méi|huài|.|Ménlíng|xiǎngle|!",
       targetLangText: "不|，|门铃|没|坏|。|门铃|响了|！",
@@ -163,7 +163,7 @@ export default [
     id: 10,
     type: "question",
     speakerInfo: null,
-    timeline: [27.043, 29.083],
+    timeline: [20.59, 22.269],
     text: {
       ipaText: "Shéi|ànle|ménlíng|?",
       targetLangText: "谁|按了|门铃|？",
@@ -179,7 +179,7 @@ export default [
     id: 11,
     type: "answer",
     speakerInfo: null,
-    timeline: [29.083, 31.3],
+    timeline: [22.269, 23.968],
     text: {
       ipaText: "Shì|Tián Zhōng|àn|de|.",
       targetLangText: "是|田中|按|的|。",
@@ -195,7 +195,7 @@ export default [
     id: 12,
     type: "question",
     speakerInfo: null,
-    timeline: [31.3, 33.923],
+    timeline: [23.968, 25.671],
     text: {
       ipaText: "Ménlíng|wèishéme|xiǎngle|?",
       targetLangText: "门铃|为什么|响了|？",
@@ -211,7 +211,7 @@ export default [
     id: 13,
     type: "answer",
     speakerInfo: null,
-    timeline: [33.923, 36.696],
+    timeline: [25.671, 27.783],
     text: {
       ipaText: "Yīnwèi|Tián Zhōng|ànle|ménlíng|.",
       targetLangText: "因为|田中|按了|门铃|。",
@@ -227,11 +227,11 @@ export default [
     id: 14,
     type: "dialogue",
     speakerInfo: {
-      avatar: "tanaka.png",
+      avatar: "tanaka.webp",
       ipaName: "Tiánzhōng",
       targetLangName: "田中",
     },
-    timeline: [37, 39],
+    timeline: [27.783, 28.978],
     text: {
       ipaText: "Nǐmen |hǎo|!",
       targetLangText: "你们|好|!",
@@ -242,5 +242,125 @@ export default [
       alt: "",
     },
     colors: ["bg-neutral-100", "text-neutral-800"],
+  },
+  {
+    id: 15,
+    type: "dialogue",
+    speakerInfo: {
+      avatar: "liming.jpeg",
+      ipaName: "Lǐ Míng",
+      targetLangName: "李明",
+    },
+    timeline: [28.978, 39.638],
+    text: {
+      ipaText:
+        "Lái|,|wǒ|gěi|nǐmen|jièshào|. |Dàwèi|,|zhè|shì|Tián Zhōng|,|tā|shì|wǒ|de|péngyǒu|,|yě|shì|wǒmen|dàxué|de|xuéshēng|.|Tián Zhōng|,|zhè|shì|Dàwèi|,|cóng|Yīngguó|lái|.",
+      targetLangText:
+        "来|，|我|给|你们|介绍|。|大卫|，|这|是|田中|，|他|是|我|的|朋友|，|也|是|我们|大学|的|学生|。|田中|，|这|是|大卫|，|从|英国|来|。",
+      translationText:
+        "Li Ming: Let me introduce you. David, this is Tanaka. He is my friend and also a student from our university. Tanaka, this is David, who is from England.",
+    },
+    img: {
+      src: "",
+      alt: "",
+    },
+    colors: ["bg-neutral-100", "text-neutral-800"],
+  },
+  {
+    id: 16,
+    type: "question",
+    speakerInfo: null,
+    timeline: [39.638, 41.653],
+    text: {
+      ipaText: "Dàwèi|shì|Lǐ Míng|de|péngyǒu|ma|?",
+      targetLangText: "大卫|是|李明|的|朋友|吗|？",
+      translationText: "Is David a friend of Li Ming?",
+    },
+    img: {
+      src: "",
+      alt: "",
+    },
+    colors: null,
+  },
+  {
+    id: 17,
+    type: "answer",
+    speakerInfo: null,
+    timeline: [41.653, 46.151],
+    text: {
+      ipaText:
+        "Bù|,|Dàwèi|bù shì|Lǐ Míng|de|péngyǒu|.|Tāmen|shì|gāng|rènshí|de|.",
+      targetLangText: "不|，|大卫|不是|李明|的|朋友|。|他们|是|刚|认识|的|。",
+      translationText: "No, David is not a friend of Li Ming. They just met.",
+    },
+    img: {
+      src: "",
+      alt: "",
+    },
+    colors: null,
+  },
+  {
+    id: 18,
+    type: "question",
+    speakerInfo: null,
+    timeline: [46.151, 48.044],
+    text: {
+      ipaText: "Tián Zhōng|shì|dàxuéshēng|ma|?",
+      targetLangText: "田中|是|大学生|吗|？",
+      translationText: "Is Tanaka a college student?",
+    },
+    img: {
+      src: "",
+      alt: "",
+    },
+    colors: null,
+  },
+  {
+    id: 19,
+    type: "answer",
+    speakerInfo: null,
+    timeline: [48.044, 50.439],
+    text: {
+      ipaText: "Duì|,|Tián Zhōng|shì|dàxuéshēng|.",
+      targetLangText: "对|，|田中|是|大学生|。",
+      translationText: "Yes, Tanaka is a college student.",
+    },
+    img: {
+      src: "",
+      alt: "",
+    },
+    colors: null,
+  },
+  {
+    id: 20,
+    type: "question",
+    speakerInfo: null,
+    timeline: [50.439, 52.295],
+    text: {
+      ipaText: "Dàwèi|cóng|Yīngguó|lái|ma|?",
+      targetLangText: "大卫|从|英国|来|吗|？",
+      translationText: "Did David come from the UK?",
+    },
+    img: {
+      src: "",
+      alt: "",
+    },
+    colors: null,
+  },
+  {
+    id: 21,
+    type: "answer",
+    speakerInfo: null,
+    timeline: [52.295, 54.888],
+    text: {
+      ipaText: "Duì|,|Dàwèi|cóng|Yīngguó|lái|.",
+      targetLangText: "对|，|大卫|从|英国|来|。",
+      translationText: "Yes, David came from the UK.",
+    },
+    img: {
+      src: "",
+      alt: "",
+    },
+    colors: null,
   },
 ];
